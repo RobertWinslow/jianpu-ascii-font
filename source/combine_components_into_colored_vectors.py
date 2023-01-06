e9b0b4fe56a425e3c89a4c64870bc79b5f25cde7
RMWinslow <robertmartinwinslow@gmail.com>
2023-01-06 16:03:32 -0600

    Create combined vectors for digits and their octave shifts

--- source/combine_components_into_colored_vectors.py
+++ source/combine_components_into_colored_vectors.py
@@ -10,13 +10,47 @@
 # That's only 105 total combos. A reasonably small number.
 # Also zero width characters for sharps and flats?
 
-#%% 
+#%% IMPORTS AND PARAMETERS
 import os
 
 INPUTFOLDER = 'svg/components'
 files = os.listdir(INPUTFOLDER)
 
 OUTPUTFOLDER = 'svg/combined'
+
+
+
+COLORS = {
+    "0": "#8a8489",
+    "1": "#bf344b",
+    "2": "#ea8127",
+    "3": "#d9ae2f",
+    "4": "#82a12b",
+    "5": "#1385af",
+    "6": "#5359b5",
+    "7": "#9352a8",
+}
+COLORS_HIGH = [
+    "#8a8489",
+    "#e66980",
+    "#ff9d50",
+    "#f7ce50",
+    "#87d989",
+    "#2dbce2",
+    "#8088e2",
+    "#ce8ce3",
+]
+COLORS_LOW = [
+    "#8a8489",
+    "#87122d",
+    "#c26012",
+    "#a29812",
+    "#176a1e",
+    "#113074",
+    "#2a286f",
+    "#652277",
+]
+
 
 #%%
 
@@ -53,37 +87,71 @@ codepointDict = {
 }
 
 
-#%%
+#
 
-def createBasicSVG(character, component):
+def createBasicFile(character, component):
     svg = buildSVG([component])
     codepoint = codepointDict[character]
     with open(os.path.join(OUTPUTFOLDER,codepoint+'.svg'), 'w') as f:
         f.write(svg)
 
-createBasicSVG('#', 'sharp')
-createBasicSVG('b', 'flat')
-createBasicSVG('^', 'sharp')
-createBasicSVG('_', 'flat')
-createBasicSVG('=', 'natural')
-createBasicSVG(':', 'colon')
-createBasicSVG('|', 'bar')
-createBasicSVG('x', 'x')
-createBasicSVG('-', 'dash')
-createBasicSVG('.', 'sidedot')
-createBasicSVG('*', 'sidedot')
-createBasicSVG(',', 'downoctave')
-createBasicSVG("'", 'upoctave')
-createBasicSVG('s', 'doubleUnderline')
-createBasicSVG('q', 'underline')
-createBasicSVG('/', 'underline')
-createBasicSVG('[', 'tupletLeft')
-createBasicSVG(']', 'tupletRight')
-createBasicSVG('(', 'slurLeft')
-createBasicSVG(')', 'slurRight')
+createBasicFile('#', 'sharp')
+createBasicFile('b', 'flat')
+createBasicFile('^', 'sharp')
+createBasicFile('_', 'flat')
+createBasicFile('=', 'natural')
+createBasicFile(':', 'colon')
+createBasicFile('|', 'bar')
+createBasicFile('x', 'x')
+createBasicFile('-', 'dash')
+createBasicFile('.', 'sidedot')
+createBasicFile('*', 'sidedot')
+createBasicFile(',', 'downoctave')
+createBasicFile("'", 'upoctave')
+createBasicFile('s', 'doubleUnderline')
+createBasicFile('q', 'underline')
+createBasicFile('/', 'underline')
+createBasicFile('[', 'tupletLeft')
+createBasicFile(']', 'tupletRight')
+createBasicFile('(', 'slurLeft')
+createBasicFile(')', 'slurRight')
+
+# pass in strings of characters to put before, and characters to put after
+from itertools import permutations
+def createPermutationFiles(svgContent, core,before,after):
+    for prefix in set(permutations(before)):
+        for suffix in set(permutations(after)):
+            charsequence = ''.join(prefix+(core,)+suffix)
+            filename = '-'.join([codepointDict[c] for c in charsequence])
+            with open(os.path.join(OUTPUTFOLDER,filename+'.svg'), 'w') as f:
+                f.write(svgContent)
+
+# DIGIT LIGATURES
+for digit in ['1','2','3','4','5','6','7',]:
+    # Single underline for quaver
+    svg = buildSVG([digit,'underline'], color=COLORS[digit])
+    createPermutationFiles(svg,digit,'','/')
+    createPermutationFiles(svg,digit,'q','')
+    # double underline for a semiquaver
+    svg = buildSVG([digit,'doubleUnderline'], color=COLORS[digit])
+    createPermutationFiles(svg,digit,'','//')
+    createPermutationFiles(svg,digit,'s','')
+    # up an octave
+    svg = buildSVG([digit,'upoctave'], color=COLORS[digit])
+    createPermutationFiles(svg,digit,'',"'")
+    # up two octaves
+    svg = buildSVG([digit,'upTwo'], color=COLORS[digit])
+    createPermutationFiles(svg,digit,'',"''")
+    # down one octave
+    svg = buildSVG([digit,'downoctave'], color=COLORS[digit])
+    createPermutationFiles(svg,digit,'',",,")
+    # down two octaves
+    svg = buildSVG([digit,'downTwo'], color=COLORS[digit])
+    createPermutationFiles(svg,digit,'',",,")
 
 
 
+#%%
 
 
 
