3422900ad9e08f6216a44c63ffa55b4c54dae632
RobertWinslow <robertmartinwinslow@gmail.com>
2024-07-07 20:10:54 -0500

    Update build_black_font_from_components.py
    license clarification

--- source/build_black_font_from_components.py
+++ source/build_black_font_from_components.py
@@ -12,6 +12,8 @@ https://github.com/RobertWinslow/Simple-SVG-to-Font-with-Fontforge
 
 See here for documentation about FontForge's scripting library:
 https://fontforge.org/docs/scripting/python/fontforge.html
+
+Script (c) 2022 Robert Winslow. CC BY-SA. See: https://github.com/RobertWinslow/Simple-SVG-to-Font-with-Fontforge
 '''
 
 
